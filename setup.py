4f77cab2512895015ec19c97673872f55f4db88f
nathants <me@nathants.com>
2019-12-26 21:52:51 -0800

    naming things

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='lz4frame-cffi',
+    name='cffi-lz4frame',
     version='0.0.0',
     author='nathants',
     author_email='me@nathants.com',
-    url='http://github.com/nathants/lz4frame-cffi/',
+    url='http://github.com/nathants/cffi-lz4frame/',
     packages=['lz4frame'],
     install_requires=['cffi>=1.0.0'],
     cffi_modules=["lz4frame/__init__.py:ffibuilder"],
